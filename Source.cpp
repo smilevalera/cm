33731b6e33efe7f68e8d405c158c5dcedfaf0560
smilevalera <91686121+smilevalera@users.noreply.github.com>
2021-10-21 17:06:43 +0300

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,14 @@
+/*валера гилёв п43021*/
+#include <iostream>
+using namespace std;
+int main() {
+	
+	int a, b;
+	cin >> a >> b;
+	if (a == 20 && b == 42)cout << "priniat\n";
+	else if (a != 20 && b != 42)cout << "ne priniat\n";
+
+
+
+
+}
